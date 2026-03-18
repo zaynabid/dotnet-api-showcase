cf6752e20ab9c6e21cca66360140319a381341d1
Zayn <zaynabid32@gmail.com>
2026-03-18 05:09:38 +0000

    feat(project-01): Finalize DI registration and app configuration

--- 01-product-catalog-api/ProductCatalogAPI/Program.cs
+++ 01-product-catalog-api/ProductCatalogAPI/Program.cs
@@ -1,4 +1,14 @@
 
+using Microsoft.EntityFrameworkCore;
+using ProductCatalogAPI.Data;
+using ProductCatalogAPI.Data.Seed;
+using ProductCatalogAPI.Middleware;
+using ProductCatalogAPI.Repositories;
+using ProductCatalogAPI.Repositories.Interfaces;
+using ProductCatalogAPI.Services;
+using ProductCatalogAPI.Services.Interfaces;
+using Scalar.AspNetCore;
+
 namespace ProductCatalogAPI
 {
     public class Program
@@ -7,21 +17,46 @@ namespace ProductCatalogAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            // Adding services to the container
+            
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+            // OpenAPI
             builder.Services.AddOpenApi();
+            
+            // Database
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            // Repositories
+            builder.Services.AddScoped<IProductRepository, ProductRepository>();
+            builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+            // Services
+            builder.Services.AddScoped<IProductService, ProductService>();
+            builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            using (var scope = app.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                context.Database.EnsureCreated();
+                DataSeeder.Seed(context);
+            }
+
+            // Middleware
+            app.UseMiddleware<ExceptionHandlingMiddleware>();
+
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.MapScalarApiReference();
             }
 
             app.UseHttpsRedirection();
+            app.UseAuthorization();
             app.MapControllers();
 
             app.Run();
